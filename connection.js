c5f12592b84e1742707043b982a53dc16a77b68f
Anshuman Singh <thakuranshumansingh945@gmail.com>
2023-08-08 23:23:01 +0530

    .env file added

--- connection.js
+++ connection.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
+require('dotenv').config(); // Load environment variables from .env file
 
-const password = encodeURIComponent("moundweb@123");
-const dbName = "User_Update_Form";
+const password = encodeURIComponent(process.env.DB_PASSWORD); // Retrieve the database password from environment variables
+const dbName = process.env.DB_NAME; // Retrieve the database name from environment variables
 const url = `mongodb+srv://moundwebitwebsolution:${password}@cluster0.smgwfyl.mongodb.net/${dbName}`;
 
-mongoose.connect(url)
+const options = {
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(url, options)
   .then((result) => {
     console.log("Database Connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Error",err);
   });
 
 module.exports = mongoose;
-
